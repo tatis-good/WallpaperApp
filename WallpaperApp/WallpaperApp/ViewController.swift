366e6230a25aa7073114b798cc4f2d805dde92bb
spark-02 <khasonlove@gmail.com>
2024-06-25 11:24:03 +0900

    スプラッシュ終了

---
+++ WallpaperApp/WallpaperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WallpaperApp
+//
+//  Created by spark-02 on 2024/06/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
